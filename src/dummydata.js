75a83373b81fb99826e88c10b73fdcb0e8422ae3
Joseph nkansah <133186186+Josephnkansah@users.noreply.github.com>
2024-10-01 08:00:52 -1200

    fast

---
+++ src/dummydata.js
@@ -0,0 +1,384 @@
+export const homeAbout = [
+  {
+    id: 1,
+    cover: "https://img.icons8.com/dotty/80/000000/storytelling.png",
+    title: "Online Courses",
+    desc: "Fast Teach Online offers flexible, expertly designed courses that allow you to learn new skills at your own pace.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/diploma.png",
+    title: "Earn A Certificates",
+    desc: "Stand out in the competitive job market by earning a certificate through Fast Teach Online. Our industry-recognized certificates validate your expertise and showcase your commitment to learning.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/athlete.png",
+    title: "Learn with Expert",
+    desc: "At Fast Teach Online, you’ll learn directly from industry experts who bring real-world experience and deep knowledge to every course. Our instructors are leaders in their fields, providing practical insights and guidance to help you succeed.",
+  },
+]
+export const awrapper = [
+  {
+    cover: "https://img.icons8.com/external-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto/80/ffffff/external-graduation-education-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto.png",
+    data: "3,000",
+    title: "SUCCESS STORIES",
+  },
+
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/athlete.png",
+    data: "320",
+    title: "TRUSTED TUTORS",
+  },
+  {
+    cover: "https://img.icons8.com/external-outline-icons-maxicons/80/ffffff/external-calender-insurance-outline-outline-icons-maxicons.png",
+    data: "1,000",
+    title: "SCHEDULES",
+  },
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/macbook-idea--v3.png",
+    data: "587",
+    title: "COURSES",
+  },
+]
+export const coursesCard = [
+  {
+    id: 1,
+    cover: "../images/courses/c1.png",
+    coursesName: "Introducing to Software Engineering",
+    courTeacher: [
+      {
+        dcover: "./images/pro2.webp",
+        name: "by John Smith",
+        totalTime: "50 lectures (190 hrs)",
+      },
+    ],
+    priceAll: "$100 All Course",
+    pricePer: "$15 per month",
+  },
+  {
+    id: 2,
+    cover: "../images/courses/c2.png",
+    coursesName: "Enhancing Adobe Photoshop CC 2020 Skills",
+    courTeacher: [
+      {
+        dcover: "./images/pro1.webp",
+        name: "by Ram Gurung",
+        totalTime: "30 lectures (125 hrs)",
+      },
+    ],
+    priceAll: "$200 All Course",
+    pricePer: "$25 per month",
+  },
+  {
+    id: 3,
+    cover: "../images/courses/c3.png",
+    coursesName: "HTML, CSS, and Javascript for Web Developers",
+    courTeacher: [
+      {
+        dcover: "./images/pro4.webp",
+        name: "by Saroj Nepal",
+        totalTime: "50 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "$50 All Course",
+    pricePer: "$5 per month",
+  },
+  {
+    id: 4,
+    cover: "../images/courses/c4.png",
+    coursesName: "Introducing to Programming with WordPress",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Prabin Thapa Mager",
+        totalTime: "20 lectures (20 hrs)",
+      },
+    ],
+    priceAll: "$30 All Course",
+    pricePer: "$3 per month",
+  },
+  {
+    id: 5,
+    cover: "../images/courses/c5.png",
+    coursesName: "Introducing to Programming with ReactJS",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Sunil Bishowkaram",
+        totalTime: "100 lectures (150 hrs)",
+      },
+    ],
+    priceAll: "$300 All Course",
+    pricePer: "$30 per month",
+  },
+  {
+    id: 6,
+    cover: "../images/courses/c6.png",
+    coursesName: "Learn Frontend Programming Language",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Sunil Bishowkaram",
+        totalTime: "200 lectures (300 hrs)",
+      },
+    ],
+    priceAll: "$500 All Course",
+    pricePer: "$80 per month",
+  },
+  {
+    id: 7,
+    cover: "../images/courses/c7.png",
+    coursesName: "Introducing to with HTML / CSS",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Price",
+        totalTime: "20 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "$20 All Course",
+    pricePer: "$2 per month",
+  },
+  {
+    id: 8,
+    cover: "../images/courses/c8.png",
+    coursesName: "Introducing to with JAVA",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Petter",
+        totalTime: "80 lectures (200 hrs)",
+      },
+    ],
+    priceAll: "$200 All Course",
+    pricePer: "$40 per month",
+  },
+  {
+    id: 9,
+    cover: "../images/courses/c9.png",
+    coursesName: "Introducing to with HTML / CSS / JAVASCRIPT",
+    courTeacher: [
+      {
+        dcover: "./images/back.webp",
+        name: "by Petter",
+        totalTime: "50 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "$20 All Course",
+    pricePer: "$3 per month",
+  },
+]
+export const online = [
+  {
+    cover: "./images/courses/online/o1.png",
+    hoverCover: "./images/courses/online/o1.1.png",
+    courseName: "UI/UX Design Courses",
+    course: "25 Courses",
+  },
+  {
+    cover: "./images/courses/online/o2.png",
+    hoverCover: "./images/courses/online/o2.1.png",
+    courseName: "Art & Design",
+    course: "25 Courses",
+  },
+  {
+    cover: "./images/courses/online/o3.png",
+    hoverCover: "./images/courses/online/o3.1.png",
+    courseName: "Computer Science",
+    course: "10 Courses",
+  },
+  {
+    cover: "./images/courses/online/o4.png",
+    hoverCover: "./images/courses/online/o4.1.png",
+    courseName: "History & Archeologic",
+    course: "15 Courses",
+  },
+  {
+    cover: "./images/courses/online/o5.png",
+    hoverCover: "./images/courses/online/o5.1.png",
+    courseName: "Software Engineering",
+    course: "30 Courses",
+  },
+  {
+    cover: "./images/courses/online/o6.png",
+    hoverCover: "./images/courses/online/o6.1.png",
+    courseName: "Information Software",
+    course: "60 Courses",
+  },
+  {
+    cover: "./images/courses/online/o7.png",
+    hoverCover: "./images/courses/online/o7.1.png",
+    courseName: "Health & Fitness",
+    course: "10 Courses",
+  },
+  {
+    cover: "./images/courses/online/o8.png",
+    hoverCover: "./images/courses/online/o8.1.png",
+    courseName: "Marketing",
+    course: "30 Courses",
+  },
+  {
+    cover: "./images/courses/online/o9.png",
+    hoverCover: "./images/courses/online/o9.1.png",
+    courseName: "Graphic Design",
+    course: "80 Courses",
+  },
+  {
+    cover: "./images/courses/online/o10.png",
+    hoverCover: "./images/courses/online/o10.1.png",
+    courseName: "Music",
+    course: "120 Courses",
+  },
+  {
+    cover: "./images/courses/online/o11.png",
+    hoverCover: "./images/courses/online/o11.1.png",
+    courseName: "Business Administration",
+    course: "17 Courses",
+  },
+  {
+    cover: "./images/courses/online/o12.png",
+    hoverCover: "./images/courses/online/o12.1.png",
+    courseName: "Web Management",
+    course: "17 Courses",
+  },
+]
+export const team = [
+  {
+    cover: "./images/joe.jpg",
+    name: "Ph.D Joseph Nkansah",
+    work: "DEVELOPER AND STUDENT FROM ALX",
+  },
+  {
+    cover: "./images/kevi.jpg",
+    name: "Ph.D Kelvin Owusu",
+    work: "DEVELOPER AND STUDENT FROM ALX",
+  }
+]
+export const price = [
+  {
+    name: "BASIC PLAN",
+    price: "49K",
+    desc: "Our Basic Plan is perfect for learners who want affordable access to high-quality educational content without needing premium features. This plan is designed to give you access to the essentials for a productive learning experience.",
+  },
+  {
+    name: "BEGINNER PLAN",
+    price: "79K",
+    desc: "The Beginner Plan is ideal for new learners who want structured access to essential courses and resources. This plan offers more flexibility and support to help you build foundational skills across various subjects.",
+  },
+  {
+    name: "PREMIUM PLAN",
+    price: "109k",
+    desc: "The Premium Plan is designed for serious learners and professionals who want full access to all features, courses, and personalized support. With this plan, you get everything you need to elevate your learning journey to the next level.",
+  },
+  {
+    name: "ULTIMATE PLAN",
+    price: "149K",
+    desc: "The Ultimate Plan is the most comprehensive and exclusive package, designed for those who want to unlock the full potential of their learning journey. With this plan, you gain VIP access to all resources, personalized services, and cutting-edge content for a truly immersive educational experience.",
+  },
+]
+export const faq = [
+  {
+    title: "How to Enroll This Online Courses?",
+    desc: "This is the first item's accordion body. It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+  },
+  {
+    title: "Where It is hidden by default, until the collapse?",
+    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+  },
+  {
+    title: "How It is hidden by default, until the collapse?",
+    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+  },
+  {
+    title: "How to Enroll This Online Courses?",
+    desc: "This is the first item's accordion body. It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+  },
+  {
+    title: "Where It is hidden by default, until the collapse?",
+    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+  },
+  {
+    title: "How It is hidden by default, until the collapse?",
+    desc: "It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
+  },
+]
+export const blog = [
+  {
+    id: 1,
+    type: "admin",
+    date: "JAN. 18, 2021",
+    com: "20 COMMENTS ",
+    title: "Build your Dream Python Career",
+    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    cover: "../images/c3.jpg",
+  },
+  {
+    id: 2,
+    type: "admin",
+    date: "API. 25, 2022",
+    com: "200 COMMENTS ",
+    title: "Build your Dream Software & Engineering Career",
+    desc: "Are you passionate about technology and eager to make an impact through software development? In this blog post, we’ll guide you on how to build your dream career in software engineering. Explore the essential skills you need, from programming languages like Python, Java, and JavaScript to software design principles and development methodologies such as Agile and DevOps.",
+    cover: "../images/pro 4.jpg",
+  },
+  {
+    id: 3,
+    type: "user",
+    date: "MAY. 15, 2022",
+    com: "10 COMMENTS ",
+    title: "Exploring the Latest Trends in Information Technology",
+    desc: "The IT landscape is constantly evolving. In this article, we dive into the latest trends shaping the industry, such as cloud computing, artificial intelligence, and cybersecurity advancements. Learn how you can stay ahead of the curve by enrolling in relevant courses on Fast Teach Online to enhance your knowledge and skills.",
+    cover: "../images/pro 2.jpg",
+  },
+  {
+    id: 4,
+    type: "admin",
+    date: "JAN. 02, 2022",
+    com: "3 COMMENTS ",
+    title: "Build your Dream KALI LINUX Career",
+    desc: "Ready to dive into the world of cybersecurity? In this blog post, we’ll guide you on how to build your dream career using Kali Linux, the leading platform for penetration testing and ethical hacking. Discover the fundamental skills you need to succeed, including mastering Linux commands, understanding networking, and utilizing various tools for security assessments.",
+    cover: "../images/pexels-realtoughcandy-11035476.jpg"
+  },
+  {
+    id: 5,
+    type: "admin",
+    date: "DEC. 14, 2022",
+    com: "3 COMMENTS ",
+    title: "Build your Dream NODC JS Career",
+    desc: "Are you ready to embark on an exciting journey into the world of Node.js? In this blog post, we’ll explore how you can build your dream career as a Node.js developer. Learn about the essential skills required, from mastering JavaScript and understanding asynchronous programming to working with databases and frameworks like Express. We'll also discuss the various career paths available, including roles as a backend developer, full-stack developer, and even a software engineer.",
+    cover: "../images/pexels-realtoughcandy-11035380.jpg",
+  },
+  {
+    id: 6,
+    type: "user",
+    date: "JAN. 18, 2021",
+    com: "12 COMMENTS ",
+    title: "A Beginner’s Guide to IT Fundamentals: What You Need to Know",
+    desc: "Are you new to the world of IT? This comprehensive guide covers the fundamental concepts of information technology, including hardware, software, networking, and security. Discover how Fast Teach Online's introductory courses can help you build a solid foundation and prepare you for more advanced IT topics.",
+    cover: "../images/signup.jpg",
+  },
+]
+export const testimonal = [
+  {
+    id: 1,
+    name: "ROGER SCOTT",
+    post: "MARKETING MANAGER",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    cover: "./images/pro3.webp",
+  },
+  {
+    id: 2,
+    name: "ROGER SCOTT",
+    post: "MARKETING MANAGER",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    cover: "./images/stu2.jpg",
+  },
+  {
+    id: 3,
+    name: "ROGER SCOTT",
+    post: "MARKETING MANAGER",
+    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    cover: "./images/pro sl.jpg",
+  },
+]
